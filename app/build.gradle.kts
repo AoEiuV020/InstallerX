0ef86fe71bc2a6222abd4e7c871d1be427fc20d9
AoEiuV020 <aoeiuv020@gmail.com>
2025-05-02 11:41:24 +0800

    改包名com.aoeiuv020.installer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         // 请换一个applicationId，不要和官方的任何发布版本产生冲突。
         // If you use InstallerX source code, package it into apk or other installation package format
         // Please change the applicationId to one that does not conflict with any official release.
-        applicationId = "com.rosan.installer.x.revived"
+        applicationId = "com.aoeiuv020.installer"
         namespace = "com.rosan.installer"
         minSdk = 34
         targetSdk = 35
